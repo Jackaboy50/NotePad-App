714f06bebb51d95bc1d84d44ea02b103e35d8cb2
Jackaboy50 <119588287+Jackaboy50@users.noreply.github.com>
2023-06-15 15:05:22 +0100

    Added UI elements

--- NotePad App/Form1.cs
+++ NotePad App/Form1.cs
@@ -7,6 +7,46 @@ namespace NotePad_App
             InitializeComponent();
         }
 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int[,] labelPoints = new int[,] { { 21, 35 }, { 21, 70 }, { 554, 41 } };
+            string[] labelText = new string[] { "Title:", "Text:", "List of Notes:" };
 
+            int[,] buttonPoints = new int[,] { { 70, 369 }, { 182, 369 }, { 562, 369 }, { 678, 369 } };
+            string[] buttonText = new string[] { "Save", "New", "Open", "Delete" };
+            for (int i = 0; i < 4; i++)
+            {
+                Button newbutton = new Button();
+                newbutton.Location = new Point(buttonPoints[i, 0], buttonPoints[i, 1]);
+                newbutton.Size = new Size(84, 29);
+                newbutton.Text = buttonText[i];
+                Controls.Add(newbutton);
+
+                if (i < 3)
+                {
+                    Label newLabel = new Label();
+                    newLabel.Text = labelText[i];
+                    newLabel.Location = new Point(labelPoints[i, 0], labelPoints[i, 1]);
+                    newLabel.Size = i == 2 ? new Size(100, 15) : new Size(38, 15);
+                    Controls.Add(newLabel);
+                }
+            }
+
+            TextBox titleTextBox = new TextBox();
+            titleTextBox.Location = new Point(70, 33);
+            titleTextBox.Size = new Size(438, 23);
+            Controls.Add(titleTextBox);
+
+            TextBox textBox = new TextBox();
+            textBox.Location = new Point(70, 68);
+            textBox.Size = new Size(438, 290);
+            textBox.Multiline = true;
+            Controls.Add(textBox);
+
+            ListBox noteListBox = new ListBox();
+            noteListBox.Location = new Point(554, 70);
+            noteListBox.Size = new Size(216, 289);
+            Controls.Add(noteListBox);
+        }
     }
 }
